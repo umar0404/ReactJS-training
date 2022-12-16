bc3bb2f04b271812d44d41f137c3acf4b1f18439
Umar <you@example.com>
2022-12-16 15:40:40 +0500

    localStorage is complated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,23 +1,29 @@
-import React from "react";
-import { Friend } from "../contex/Friends";
+import React, { useState } from "react";
 
 const Body = () => {
-  const [friends, setFriends] = Friend();
-  const onDelete = (id) => {
-    let res = friends.filter((fr) => fr.id !== id);
-    setFriends(res);
+  const [count, setCount] = useState(Number(localStorage.getItem("count")));
+  const plus = () => {
+    localStorage.setItem("count", count + 1);
+    setCount(count + 1);
   };
+
+  const minus = () => {
+    // when we set item as NoNprimitive we should include it with JSON.stringify()
+    // localStorage.setItem("count", JSON.stringify({ data: count - 1 }));
+    localStorage.setItem("count", count - 1);
+    setCount(count - 1);
+  };
+  // when we get item which is type nonprimitive we should include it with JSON.parse()
+  // console.log(JSON.parse(localStorage.getItem('count')));
   return (
     <div>
-      <h1>Friends {friends.length} </h1>
-      {friends.map((value) => {
-        return (
-          <h1>
-            Name:{value.name}
-            <button onClick={() => onDelete(value.id)}>Delete</button>
-          </h1>
-        );
-      })}
+      <h1>Count {count}</h1>
+      <button onClick={minus}>-</button>
+      <button onClick={plus}>+</button>
+      <button onClick={() => localStorage.removeItem("count")}>
+        Delete Count
+      </button>
+      <button onClick={() => localStorage.clear()}>Clear</button>
     </div>
   );
 };
